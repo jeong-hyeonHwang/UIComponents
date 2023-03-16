46ba2f84c1980d96756c946aaa29844ecf026867
jeonghyeon Hwang <hijihilin@gmail.com>
2023-03-17 00:27:37 +0900

    Init

---
+++ UIComponents/UIComponents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIComponents
+//
+//  Created by 황정현 on 2023/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
